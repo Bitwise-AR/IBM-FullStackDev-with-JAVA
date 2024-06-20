e63e568984ba1587ea5dcc7f58198f0be39ab28c
Ayush Raj <rajayush1412@gmail.com>
2024-06-20 20:45:53 +0530

    updates

---
+++ javascript basic/arrays.js
@@ -0,0 +1,15 @@
+let users = [1, 2, 3, 4, 5, [2, 3, 4, 5]];
+
+console.log(users[3]); // 4
+
+console.log(users[5]); // [2,3,4,5]
+// 2
+// users[5][2]
+// users[5] -- is a array
+// users[5][2]
+
+console.log(users[5][2]); // 4
+
+let arr = [1, true, "kashish", 6.44];
+
+console.log(arr[2]);
